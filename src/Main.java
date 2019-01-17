9ca9ba216651e8bb1a89610cde55747aed565057
ThirzavHemert <cahemert@avans.nl>
2019-01-17 11:17:23 +0100

    Basic GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import javax.swing.SwingUtilities;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        UserInterface ui = new UserInterface();
+        SwingUtilities.invokeLater(ui);
     }
 }
